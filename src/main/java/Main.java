a1c0535e84b7eb0fbfedfe745fc7e571936adc48
C.T.Raines.III <c.t.raines.iii@gmail.com>
2018-02-12 12:38:51 -0700

    Clifford

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+import Controller.ViewOperator;
+
+public class Main {
+    public static void main(String[] args) {
+        ViewOperator vo = new ViewOperator();
+        vo.runOperator();
+    }
+}
